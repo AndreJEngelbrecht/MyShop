d5470e82d7c15fe2fe779d23278b7749e39e014c
AndreJEngelbrecht <53150877+AndreJEngelbrecht@users.noreply.github.com>
2019-07-23 23:08:57 +0200

    Product grouping added

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,73 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories;
+
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["productCategories"] as List<ProductCategory>;
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+        public void Insert(ProductCategory pC)
+        {
+            productCategories.Add(pC);
+        }
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory ProductCategoryToUpdate = productCategories.Find(pC => pC.Id == productCategory.Id);
+            if (ProductCategoryToUpdate != null)
+            {
+                ProductCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Category not found");
+            }
+        }
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory productCategory = productCategories.Find(pC => pC.Id == Id);
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Category not found");
+            }
+        }
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            ProductCategory ProductCategoryToDelete = productCategories.Find(pC => pC.Id == Id);
+            if (ProductCategoryToDelete != null)
+            {
+                productCategories.Remove(ProductCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Category not found");
+            }
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,113 @@
+﻿using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepository contextCategory;
+
+        public ProductCategoryManagerController()
+        {
+            contextCategory = new ProductCategoryRepository();
+        }
+
+        // GET: ProductCategoryManager
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = contextCategory.Collection().ToList();
+
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                contextCategory.Insert(productCategory);
+                contextCategory.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategory = contextCategory.Find(Id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(ProductCategory productCategory, String Id)
+        {
+            ProductCategory ProductCategoryToEdit = contextCategory.Find(Id);
+            if (ProductCategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategory);
+                }
+                ProductCategoryToEdit.Category = productCategory.Category;
+                ProductCategoryToEdit.Id = productCategory.Id;
+
+                contextCategory.Commit();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+        public ActionResult Delete(string Id)
+        {
+            ProductCategory ProductCategoryToDelete = contextCategory.Find(Id);
+            if (ProductCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(ProductCategoryToDelete);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory ProductCategoryToDelete = contextCategory.Find(Id);
+            if (ProductCategoryToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                contextCategory.Delete(Id);
+                contextCategory.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
